fc320ccd6ab7fbea845f696a6d589471298f1a13
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-09-02 20:25:18 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello!</h1>'
+    return '<h1>Hello {}!</h1>'.format("nono")
+
+
+@app.route('/json')
+def json():
+    return jsonify({
+        'key': 'value',
+        'key2': [1, 2, 3, 4, 5]
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
