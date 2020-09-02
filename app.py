2ee77bda03e4125ae42e1ba102d5712fd06f1ba6
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-09-02 20:45:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,36 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return '<h1>Hello {}!</h1>'.format("nono")
+
+
+@app.route('/home', methods=['POST', 'GET'], defaults={'name': 'Mr.X'})
+@app.route('/home/<int:name>', methods=['POST', 'GET'])
+def home(name):
+    return '<h1>Hello {}, You are on the home page!</h1>'.format(name)
+
+
+@app.route('/query')
+def query():
+    name = request.args.get('name')
+    location = request.args.get('location')
+    return '<h1>Hi {}. You are from {}. You are on the query page!</h1>'.format(name, location)
+
+
+@app.route('/theform')
+def theForm():
+    return '''<form method="POST" action="/process">
+    <input type="text" name="name">
+    <input type="text" name="location">
+    <input type="submit">
+    </form>
+    '''
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    location = request.form['location']
+    return '<h1>Hi {}. You are from {}. You have submitted the form successfully!</h1>'.format(name, location)
 
 
 @app.route('/json')
